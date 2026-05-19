06150b376cdd124fb4ab9fd79fa2e89cce96334e
Alexandra Lara <alexandrailp.r@gmail.com>
2026-05-19 16:40:16 -0400

    feat: se mejora el flujo de autenticación con UserContext

--- frontend/src/context/UserContext.jsx
+++ frontend/src/context/UserContext.jsx
@@ -5,12 +5,33 @@ export const UserContext = createContext();
 const UserProvider = ({ children }) => {
   const [token, setToken] = useState(true);
 
+  const [email, setEmail] = useState("pizza@mamamia.com");
+
+  const login = (userEmail) => {
+    setToken(true);
+    setEmail(userEmail);
+  };
+
+  const register = (userEmail) => {
+    setToken(true);
+    setEmail(userEmail);
+  };
+
   const logout = () => {
     setToken(false);
+    setEmail("");
   };
 
   return (
-    <UserContext.Provider value={{ token, logout }}>
+    <UserContext.Provider
+      value={{
+        token,
+        email,
+        login,
+        register,
+        logout,
+      }}
+    >
       {children}
     </UserContext.Provider>
   );
